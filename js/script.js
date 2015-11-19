df458ad87c4d9d58af745d9ad37f6d9f7dc231c6
a.bernard <a.bernard@e-deal.com>
2015-11-19 10:30:47 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Rectus_29
+ * Date: 04/12/14
+ * Time: 15:20
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    $('section').css({
+        'min-height': $(window).height()
+    });
+})
+
